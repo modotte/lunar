018137c8277b32f2e21d8531ea505ffb0a7c4932
modotte <runecroon@gmail.com>
2022-08-03 22:44:44 +0800

    Add basic navs

--- src/main.rs
+++ src/main.rs
@@ -5,41 +5,93 @@ use yewdux::prelude::*;
 use serde::{Deserialize, Serialize};
 
 #[derive(Default, Clone, PartialEq, Eq, Deserialize, Serialize, Store)]
+enum Screen {
+    #[default]
+    MainNavigation,
+    MainMenu,
+    NewCharacter,
+}
+
+#[derive(Default, Clone, PartialEq, Eq, Deserialize, Serialize, Store)]
 #[store(storage = "local")]
 struct Model {
-    count: u32,
+    current_screen: Screen,
 }
 
 enum Msg {
-    Increment,
-    Decrement,
+    SwitchScreen(Screen),
 }
 
 impl Reducer<Model> for Msg {
     fn apply(&self, mut model: Rc<Model>) -> Rc<Model> {
         let state = Rc::make_mut(&mut model);
         match self {
-            Msg::Increment => state.count += 1,
-            Msg::Decrement => state.count -= 1,
+            Msg::SwitchScreen(s) => state.current_screen = s.to_owned(),
         };
 
         model
     }
 }
 
+fn root_container(model: Rc<Model>, dispatch: &Dispatch<Model>, view: Html) -> Html {
+    html! {
+        <div>
+        { view }
+        </div>
+    }
+}
+
+fn show_main_menu(model: Rc<Model>, dispatch: &Dispatch<Model>) -> Html {
+    root_container(
+        model,
+        dispatch,
+        html! {
+            <>
+            <p>{"Hello world!"}</p>
+            <button onclick={dispatch.apply_callback(|_| Msg::SwitchScreen(Screen::NewCharacter))}>{"Start"}</button>
+            </>
+        },
+    )
+}
+
+fn show_new_character(model: Rc<Model>, dispatch: &Dispatch<Model>) -> Html {
+    root_container(
+        model,
+        dispatch,
+        html! {
+            <>
+            <h2>{"New character"}</h2>
+            <button onclick={dispatch.apply_callback(|_| Msg::SwitchScreen(Screen::MainNavigation))}>{"Confirm & Continue"}</button>
+            <button onclick={dispatch.apply_callback(|_| Msg::SwitchScreen(Screen::MainMenu))}>{"Back"}</button>
+            </>
+        },
+    )
+}
+
+fn show_main_navigation(model: Rc<Model>, dispatch: &Dispatch<Model>) -> Html {
+    root_container(
+        model,
+        dispatch,
+        html! {
+            <>
+            <h2>{"Navigation page"}</h2>
+            <button onclick={dispatch.apply_callback(|_| Msg::SwitchScreen(Screen::MainMenu))}>{"Back to main menu"}</button>
+            </>
+        },
+    )
+}
+
 #[function_component]
-fn App() -> Html {
+fn View() -> Html {
     let (model, dispatch) = use_store::<Model>();
 
-    html! {
-        <>
-        <p>{ model.count }</p>
-            <button onclick={dispatch.apply_callback(|_| Msg::Increment)}>{"+1"}</button>
-            <button onclick={dispatch.apply_callback(|_| Msg::Decrement)}>{"-1"}</button>
-        </>
+    match model.current_screen {
+        Screen::MainMenu => show_main_menu(model, &dispatch),
+        Screen::NewCharacter => show_new_character(model, &dispatch),
+        Screen::MainNavigation => show_main_navigation(model, &dispatch),
     }
 }
 
 fn main() {
-    yew::Renderer::<App>::new().render();
+    yew::Renderer::<View>::new().render();
 }
