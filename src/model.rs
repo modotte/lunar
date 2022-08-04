c8e4b66cc4ce81e21d34117f4ae90e0543110a0a
modotte <runecroon@gmail.com>
2022-08-05 04:31:54 +0800

    Modularize codebase

---
+++ src/model.rs
@@ -0,0 +1,172 @@
+use std::collections::HashMap;
+use std::fmt::Display;
+use yewdux::prelude::*;
+
+use enum_display_derive::Display;
+use serde::{Deserialize, Serialize};
+
+#[derive(Default, Clone, PartialEq, Eq, Deserialize, Serialize, Store)]
+pub enum Screen {
+    #[default]
+    MainNavigation,
+    About,
+    MainMenu,
+    NewCharacter,
+    Dock,
+    DockMarket,
+}
+
+#[derive(Default, Display, Clone, PartialEq, Eq, Deserialize, Serialize, Store)]
+pub enum Nationality {
+    #[default]
+    British,
+    Spanish,
+    French,
+}
+
+#[derive(Default, Clone, PartialEq, Eq, Deserialize, Serialize, Store)]
+pub struct Cargo {
+    pub name: String,
+    pub description: String,
+    pub price: u32,
+    pub unit: u32,
+}
+
+#[derive(Clone, PartialEq, Eq, Deserialize, Serialize, Store)]
+pub struct CargoItems {
+    pub wood: Cargo,
+    pub sugar: Cargo,
+}
+
+impl Default for CargoItems {
+    fn default() -> Self {
+        CargoItems {
+            wood: Cargo {
+                price: 40,
+                ..Default::default()
+            },
+            sugar: Cargo {
+                price: 60,
+                ..Default::default()
+            },
+        }
+    }
+}
+
+#[derive(Default, Clone, PartialEq, Eq, Deserialize, Serialize, Store)]
+pub enum ShipClass {
+    Cutter,
+    #[default]
+    Sloop,
+    Brig,
+    Junk,
+    Galleon,
+    Frigate,
+}
+
+#[derive(Default, Clone, PartialEq, Eq, Deserialize, Serialize, Store)]
+pub struct Ship {
+    pub name: String,
+    pub class: ShipClass,
+    pub cargo: CargoItems,
+    pub crew: u32,
+    pub nationality: Nationality,
+}
+
+#[derive(Default, Clone, PartialEq, Eq, Deserialize, Serialize, Store)]
+pub enum PortPopulation {
+    Small,
+    #[default]
+    Medium,
+    Large,
+    Huge,
+}
+
+#[derive(Clone, PartialEq, Eq, Deserialize, Serialize, Store)]
+pub struct Port {
+    pub name: String,
+    pub description: String,
+    pub population: PortPopulation,
+    pub nationality: Nationality,
+    pub cargo: CargoItems,
+}
+
+impl Default for Port {
+    fn default() -> Self {
+        Port {
+            cargo: CargoItems {
+                wood: Cargo {
+                    unit: 35,
+                    ..Default::default()
+                },
+                sugar: Cargo {
+                    unit: 70,
+                    ..Default::default()
+                },
+            },
+            name: String::from(""),
+            description: String::from(""),
+            population: PortPopulation::default(),
+            nationality: Nationality::default(),
+        }
+    }
+}
+
+#[derive(Default, Clone, PartialEq, Eq, Deserialize, Serialize, Store)]
+pub struct Player {
+    pub name: String,
+    pub age: u8,
+    pub coins: u32,
+    pub ship: Ship,
+}
+
+#[derive(Default, Copy, Display, Hash, Clone, PartialEq, Eq, Deserialize, Serialize, Store)]
+pub enum Location {
+    Barbados,
+    #[default]
+    PortRoyal,
+    Nassau,
+}
+
+#[derive(Clone, PartialEq, Eq, Deserialize, Serialize, Store)]
+#[store(storage = "local")]
+pub struct Model {
+    pub current_screen: Screen,
+    pub player: Player,
+    pub locations: HashMap<Location, Port>,
+    pub current_location: Location,
+}
+
+// Initializer for our whole model at launch
+impl Default for Model {
+    fn default() -> Self {
+        Self {
+            player: Player {
+                name: String::from("Jameson"),
+                age: 18,
+                coins: 1000,
+                ship: Ship {
+                    name: String::from("The Duchess"),
+                    crew: 12,
+                    ..Default::default()
+                },
+            },
+            locations: HashMap::from([
+                (Location::Barbados, Port::default()),
+                (Location::PortRoyal, Port::default()),
+                (Location::Nassau, Port::default()),
+            ]),
+            current_screen: Screen::default(),
+            current_location: Location::default(),
+        }
+    }
+}
+
+pub enum Msg {
+    SwitchScreen(Screen),
+    SwitchPlayerLocation(Location),
+    WoodBought(Location),
+    SugarBought(Location),
+    WoodSold(Location),
+    SugarSold(Location),
+}
---
+++ src/view.rs
@@ -0,0 +1,151 @@
+use std::rc::Rc;
+use web_sys::HtmlInputElement;
+use yew::prelude::*;
+use yewdux::prelude::*;
+
+use crate::model::*;
+
+fn root_container(view: Html) -> Html {
+    html! {
+        <div>
+        { view }
+        </div>
+    }
+}
+
+fn onclick_switch_screen(dispatch: &Dispatch<Model>, screen: Screen, name: &str) -> Html {
+    html! {
+        <button onclick={dispatch.apply_callback(move |_| Msg::SwitchScreen(screen.to_owned()))}>{name}</button>
+    }
+}
+
+fn debug_header(dispatch: &Dispatch<Model>) -> Html {
+    html! {
+        <div>
+            <h1>{"Lunar"}</h1>
+            { onclick_switch_screen(dispatch, Screen::MainMenu, "Back to main menu") }
+            <hr/>
+        </div>
+    }
+}
+
+fn show_about(model: Rc<Model>, dispatch: &Dispatch<Model>) -> Html {
+    root_container(html! {
+        <>
+        { debug_header(dispatch) }
+        <p>{"About screen"}</p>
+        { onclick_switch_screen(dispatch, Screen::MainMenu, "Back") }
+        </>
+    })
+}
+
+fn show_main_menu(model: Rc<Model>, dispatch: &Dispatch<Model>) -> Html {
+    root_container(html! {
+        <>
+        <p>{"Hello world!"}</p>
+        { onclick_switch_screen(dispatch, Screen::NewCharacter, "Start") }
+        <br/>
+        { onclick_switch_screen(dispatch, Screen::About, "About") }
+        </>
+    })
+}
+
+fn show_new_character(model: Rc<Model>, dispatch: &Dispatch<Model>) -> Html {
+    root_container(html! {
+        <>
+        { debug_header(dispatch) }
+        <h2>{"New character"}</h2>
+        <br/>
+        <label>{"Name"}</label>
+        <br/>
+        <input placeholder="Jefferson" required=true type="text" value={model.player.name.to_owned()}
+            onkeypress={dispatch.reduce_mut_callback_with(move |model, e: KeyboardEvent| {
+                if e.key() == "Enter" {
+                    let input: HtmlInputElement = e.target_unchecked_into();
+
+                    model.player.name = input.value()
+                }
+            })}
+        />
+        <br/>
+        { onclick_switch_screen(dispatch, Screen::MainNavigation, "Continue") }
+        { onclick_switch_screen(dispatch, Screen::MainMenu, "Back") }
+        </>
+    })
+}
+
+fn show_main_navigation(model: Rc<Model>, dispatch: &Dispatch<Model>) -> Html {
+    root_container(html! {
+        <>
+        { debug_header(dispatch) }
+        <h2>{"Navigation page"}</h2>
+
+        { onclick_switch_screen(dispatch, Screen::Dock, "Dock") }
+        { onclick_switch_screen(dispatch, Screen::MainMenu, "Back to main menu") }
+
+        <hr/>
+
+        <button onclick={dispatch.apply_callback(|_| Msg::SwitchPlayerLocation(Location::Barbados) )}>{ Location::Barbados }</button>
+        <button onclick={dispatch.apply_callback(|_| Msg::SwitchPlayerLocation(Location::PortRoyal) )}>{Location::PortRoyal}</button>
+        <button onclick={dispatch.apply_callback(|_| Msg::SwitchPlayerLocation(Location::Nassau) )}>{Location::Nassau}</button>
+
+        </>
+    })
+}
+
+fn show_dock(model: Rc<Model>, dispatch: &Dispatch<Model>) -> Html {
+    html! {
+        <>
+        { debug_header(dispatch) }
+        <h2>{"Dock screen"}</h2>
+
+        { onclick_switch_screen(dispatch, Screen::DockMarket, "Market") }
+        </>
+    }
+}
+
+fn cargo_market(model: Rc<Model>, dispatch: &Dispatch<Model>) -> Html {
+    let current_location = model.current_location.clone();
+    html! {
+        <div>
+            <ul>
+                <li>
+                    <p>{"Wood"}</p>
+                    <button onclick={dispatch.apply_callback(move |_| Msg::WoodBought(current_location))}>{"Buy 1"}</button>
+                    <button onclick={dispatch.apply_callback(move |_| Msg::WoodSold(current_location))}>{"Sell 1"}</button>
+                </li>
+
+                <li>
+                    <p>{"Sugar"}</p>
+                    <button onclick={dispatch.apply_callback(move |_|  Msg::SugarBought(current_location))}>{"Buy 1"}</button>
+                    <button onclick={dispatch.apply_callback(move |_| Msg::SugarSold(current_location))}>{"Sell 1"}</button>
+                </li>
+            </ul>
+        </div>
+    }
+}
+
+fn show_dock_market(model: Rc<Model>, dispatch: &Dispatch<Model>) -> Html {
+    html! {
+        <>
+        { debug_header(dispatch) }
+        <h2>{"Market screen"}</h2>
+
+        { cargo_market(model, dispatch) }
+        </>
+    }
+}
+
+#[function_component]
+pub fn View() -> Html {
+    let (model, dispatch) = use_store::<Model>();
+
+    match model.current_screen {
+        Screen::About => show_about(model, &dispatch),
+        Screen::MainMenu => show_main_menu(model, &dispatch),
+        Screen::NewCharacter => show_new_character(model, &dispatch),
+        Screen::MainNavigation => show_main_navigation(model, &dispatch),
+        Screen::Dock => show_dock(model, &dispatch),
+        Screen::DockMarket => show_dock_market(model, &dispatch),
+    }
+}
