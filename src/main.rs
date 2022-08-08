2689602bff2e62db436c1c503f916aa9375d097c
modotte <runecroon@gmail.com>
2022-08-09 00:26:46 +0800

    Qualify model module

--- src/main.rs
+++ src/main.rs
@@ -6,25 +6,24 @@ mod model;
 mod view;
 
 use chrono::Duration;
-use model::{Cargo, Enemy, Model, Msg, Nationality, Player, ShipClass, MINIMUM_SHIP_HULL};
 use rand::seq::SliceRandom;
 
-fn is_cargo_space_available(p: &Player) -> bool {
+fn is_cargo_space_available(p: &model::Player) -> bool {
     p.ship.cargos.total_unit() < p.ship.cargos_capacity
 }
 
-fn is_valid_buy(p: &Player, port_cargo: &Cargo) -> bool {
+fn is_valid_buy(p: &model::Player, port_cargo: &model::Cargo) -> bool {
     p.coins > port_cargo.price && is_cargo_space_available(p)
 }
 
-impl Reducer<Model> for Msg {
-    fn apply(&self, mut model: Rc<Model>) -> Rc<Model> {
+impl Reducer<model::Model> for model::Msg {
+    fn apply(&self, mut model: Rc<model::Model>) -> Rc<model::Model> {
         let state = Rc::make_mut(&mut model);
 
         // TODO: Send alert on insufficient fund or empty cargo unit
         match self {
-            Msg::ResetModel => {
-                let defmodel = Model::default();
+            model::Msg::ResetModel => {
+                let defmodel = model::Model::default();
                 state.date = defmodel.date;
                 state.current_screen = defmodel.current_screen;
                 state.current_port_location = defmodel.current_port_location;
@@ -32,7 +31,7 @@ impl Reducer<Model> for Msg {
                 state.ports = defmodel.ports;
                 state.enemy = defmodel.enemy;
             }
-            Msg::SwitchScreen(s) => match s {
+            model::Msg::SwitchScreen(s) => match s {
                 model::Screen::MainNavigation => {
                     state.enemy = None;
                     state.current_screen = s.to_owned()
@@ -45,14 +44,14 @@ impl Reducer<Model> for Msg {
                         "Morning Star",
                         "Blue Ocean",
                     ];
-                    let new_enemy = Enemy {
+                    let new_enemy = model::Enemy {
                         ship: model::Ship {
                             name: names
                                 .choose(&mut rand::thread_rng())
                                 .unwrap_or(&names[0])
                                 .to_string(),
-                            class: ShipClass::Sloop,
-                            nationality: Nationality::British,
+                            class: model::ShipClass::Sloop,
+                            nationality: model::Nationality::British,
                             crew: 7,
                             crew_capacity: 8,
                             hull: 8,
@@ -69,7 +68,7 @@ impl Reducer<Model> for Msg {
                 _ => state.current_screen = s.to_owned(),
             },
 
-            Msg::SwitchPlayerLocation(l) => {
+            model::Msg::SwitchPlayerLocation(l) => {
                 if state.current_port_location != *l {
                     let days: Vec<i64> = (1..9).collect();
                     state.date.add_assign(Duration::days(
@@ -81,7 +80,7 @@ impl Reducer<Model> for Msg {
 
             // We don't need to pattern match the get_mut(l)
             // because of enum as hashmap key usage
-            Msg::BuyWood(l) => {
+            model::Msg::BuyWood(l) => {
                 let mut port_cgs = &mut state.ports.get_mut(l).unwrap().cargos;
                 if is_valid_buy(&state.player, &port_cgs.wood) {
                     state.player.coins -= &port_cgs.wood.price;
@@ -89,7 +88,7 @@ impl Reducer<Model> for Msg {
                     state.player.ship.cargos.wood.unit += 1;
                 }
             }
-            Msg::BuySugar(l) => {
+            model::Msg::BuySugar(l) => {
                 let mut port_cgs = &mut state.ports.get_mut(l).unwrap().cargos;
                 if is_valid_buy(&state.player, &port_cgs.sugar) {
                     state.player.coins -= port_cgs.sugar.price;
@@ -97,7 +96,7 @@ impl Reducer<Model> for Msg {
                     state.player.ship.cargos.sugar.unit += 1;
                 }
             }
-            Msg::SellWood(l) => {
+            model::Msg::SellWood(l) => {
                 let mut port_wood = &mut state.ports.get_mut(l).unwrap().cargos.wood;
                 if state.player.ship.cargos.wood.unit != 0 {
                     state.player.coins += port_wood.price;
@@ -105,7 +104,7 @@ impl Reducer<Model> for Msg {
                     state.player.ship.cargos.wood.unit -= 1;
                 }
             }
-            Msg::SellSugar(l) => {
+            model::Msg::SellSugar(l) => {
                 let mut port_sugar = &mut state.ports.get_mut(l).unwrap().cargos.sugar;
                 if state.player.ship.cargos.sugar.unit != 0 {
                     state.player.coins += port_sugar.price;
@@ -113,7 +112,7 @@ impl Reducer<Model> for Msg {
                     state.player.ship.cargos.sugar.unit -= 1;
                 }
             }
-            Msg::SkirmishChaseClose => {
+            model::Msg::SkirmishChaseClose => {
                 if let Some(enemy) = &mut state.enemy {
                     match enemy.distance {
                         model::EnemyDistance::Escape => enemy.distance = model::EnemyDistance::Far,
@@ -125,7 +124,7 @@ impl Reducer<Model> for Msg {
                     }
                 }
             }
-            Msg::SkirmishChaseDistant => {
+            model::Msg::SkirmishChaseDistant => {
                 if let Some(enemy) = &mut state.enemy {
                     match enemy.distance {
                         model::EnemyDistance::Escape => {
@@ -140,9 +139,9 @@ impl Reducer<Model> for Msg {
                     }
                 }
             }
-            Msg::SkirmishChaseBroadside => {
+            model::Msg::SkirmishChaseBroadside => {
                 if let Some(enemy) = &mut state.enemy {
-                    if enemy.ship.hull < MINIMUM_SHIP_HULL.into() {
+                    if enemy.ship.hull < model::MINIMUM_SHIP_HULL.into() {
                         state.current_screen = model::Screen::MainNavigation;
                     } else {
                         match enemy.distance {
@@ -154,19 +153,19 @@ impl Reducer<Model> for Msg {
                     }
                 }
             }
-            Msg::SkirmishBattleSwingSword => {
+            model::Msg::SkirmishBattleSwingSword => {
                 if let Some(enemy) = &mut state.enemy {
                     enemy.ship.crew -= 1;
                 }
             }
-            Msg::SkirmishBattleShootFalconet => {
+            model::Msg::SkirmishBattleShootFalconet => {
                 if let Some(enemy) = &mut state.enemy {
                     enemy.ship.crew -= 2;
                 }
             }
 
             // TODO: Subtract with hull and other items cost
-            Msg::RepairShip(c) => {
+            model::Msg::RepairShip(c) => {
                 let defship = model::Model::default().player.ship;
                 state.player.ship.hull = defship.hull;
                 state.player.ship.cannons = defship.cannons;
